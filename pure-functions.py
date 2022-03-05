30c51981e4c6d28dc827bc0331bd97a6812f8d7a
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-05 00:56:04 -0600

    Add files via upload

---
+++ pure-functions.py
@@ -0,0 +1,25 @@
+def multiply_by2(li):
+    new_list = []
+    for item in li:
+        new_list.append(item*2)
+    return print(new_list)
+
+new_list = ''
+print(multiply_by2([1,2,3]))
+
+# pure function keep our data and our function different
+
+wizard = {
+    'name': 'Merlin',
+    'power': 50
+}
+
+def attack(character):
+    new_list = []
+    for item in li:
+        new_list.append(item*2)
+    return new_list
+
+print(multiply_by2([1,2,3]))
+
+
